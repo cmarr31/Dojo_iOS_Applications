509cfeacb323bb85136db7ca8b5369f073058289
cmarr31 <chris@sitefuelusa.com>
2018-08-07 03:31:58 -0500

    Graphics Primitives assignment.

--- week_2/GraphicsPrimitives/GraphicsPrimitives/ViewController.swift
+++ week_2/GraphicsPrimitives/GraphicsPrimitives/ViewController.swift
@@ -11,13 +11,50 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //Create a Point struct that has two properties called "X" & "Y" both of which are Doubles
+        struct Point {
+            var x: Double
+            var y: Double
+        }
+        let myPoint = Point(x: 2.1, y: 3.4)
+        
+        //Create a Line struct that has a "Start" and "End" property both of type Point
+        struct Line {
+            var start: Point
+            var end: Point
+            var length: Double
+            func returnLength() -> Double {
+                return length
+            }
+        }
+        
+        //In the Line struct add a function to return the Length of the created line as a Double.
+        
+        //Create a Triangle Struct that contains a property called "Points" which is an array of type Point
+        //(Optional) Notice this optional can be quite hard! Add a function to the Triangle to get the area of the triangle built with three provided points and return the value as a Double.
+        //struct Triangle {
+        //    var Points = [Point]()
+        //    func returnArea() -> Double {
+        //
+        //        return area
+        //    }
+        //}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
 
